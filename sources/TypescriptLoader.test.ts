f07042a92ed12f6cabad3c50ef0a71758025229e
Eric Miffre <eric.miffre@gmail.com>
2023-04-06 15:59:14 +0200

    [Tests] Added a test case for the parsing of a JS file

--- sources/TypescriptLoader.test.ts
+++ sources/TypescriptLoader.test.ts
@@ -9,7 +9,7 @@ import { processorForTypescript } from "./TypescriptLoader.ts"
 
 const DATA_BASE_PATH = "tests/"
 
-Deno.test("ts loader", async () =>
+Deno.test("ts loader, ts file", async () =>
 {
 	const dir = path.resolve(DATA_BASE_PATH)
 
@@ -31,6 +31,39 @@ Deno.test("ts loader", async () =>
 	})
 	
 	const node = walker.pathAsStringToNode('one.ts')
+	assert(node && node.kind == 'FILE')
+	
+	const payload = node.payload
+	assert(payload)
+	
+	const statement = payload.ast.statements[0]
+	assert(statement)
+
+	assert(statement.kind == ts.SyntaxKind.FunctionDeclaration)
+});
+
+Deno.test("ts loader, js file", async () =>
+{
+	const dir = path.resolve(DATA_BASE_PATH)
+
+	const walker = new Walker<JsPayload|TsPayload>()
+	await walker.init(dir,
+	{
+		async onFileNodeEnter (node, _, filepath)
+		{
+			// filepath is provided only on first pass
+			assert(filepath)
+			
+			const content = await Deno.readTextFile(filepath)
+
+			if (Walker.matches.glob(filepath, "**/*.js"))
+			{
+				node.payload = processorForTypescript(content)
+			}
+		}
+	})
+	
+	const node = walker.pathAsStringToNode('two.js')
 	assert(node && node.kind == 'FILE')
 	
 	const payload = node.payload
